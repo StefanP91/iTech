6b625dc44bbd62fed07af100fe46116cd9fe6020
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2025-05-11 12:10:01 +0200

    Finish Parents Corner

--- parents-corner/Scripts/main.js
+++ parents-corner/Scripts/main.js
@@ -19,3 +19,20 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+// Accordion button
+document.querySelectorAll('.accordion-button').forEach(button => {
+    button.addEventListener('click', function () {
+  
+      document.querySelectorAll('.accordion-button .icon').forEach(icon => {
+        icon.textContent = '+';
+      });
+  
+      const icon = this.querySelector('.icon');
+      if (this.classList.contains('collapsed')) {
+        icon.textContent = '+';
+      } else {
+        icon.textContent = '-';
+      }
+    });
+});
